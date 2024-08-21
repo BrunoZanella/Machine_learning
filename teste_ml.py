f89b01b0582b1da29886cb70ad7db2a8f096bf63
BrunoZanella <zanella_bruno7@gmail.com>
2024-08-21 14:17:47 -0300

    redimensionar objetos

--- teste_ml.py
+++ teste_ml.py
@@ -450,7 +450,7 @@ async def main():
 
     # Adicionar a logo no canto esquerdo
     with placeholder_logo.container():
-        st.logo('../imagens/log_brg_novo_branco_2.png')
+        st.logo('log_brg_novo_branco_2.png')
     #    st.image('../imagens/log_brg_novo_branco_2.png', width=150)
         st.markdown('<div class="main-container">', unsafe_allow_html=True)
         st.markdown('<h2>Relatório de Quebras Diário</h2>', unsafe_allow_html=True)
